269f96655a039665c8b7458e75f7fe57d901fa67
Jason <jason@zgwit.com>
2023-07-22 01:00:35 +0800

    配置文件优化

--- config/config.go
+++ config/config.go
@@ -8,8 +8,24 @@ import (
 )
 
 func Load() {
-	_ = log.Load()
-	_ = web.Load()
-	_ = db.Load()
-	_ = mqtt.Load()
+
+	err := log.Load()
+	if err != nil {
+		_ = log.Store()
+	}
+
+	err = web.Load()
+	if err != nil {
+		_ = web.Store()
+	}
+
+	err = db.Load()
+	if err != nil {
+		_ = db.Store()
+	}
+
+	err = mqtt.Load()
+	if err != nil {
+		_ = mqtt.Store()
+	}
 }
